191d188c62d21e4ecfdd4ac32e73f7c1daeb92bb
baophongbui <phongtin765@gmail.com>
2025-05-30 14:54:15 +0700

    refactor_code

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:gplx/features/home/presentation/screens/home_screen.dart';
 import 'package:gplx/features/settings/presentation/screens/settings_screen.dart';
 import 'package:gplx/features/signs/presentation/screens/traffic_signs_screen.dart';
-import 'package:gplx/features/test_sets/views/test_sets_screen.dart';
 
 class AppRoutes {
   static const String home = '/';
